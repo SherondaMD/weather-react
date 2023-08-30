cb4892e522f818ef775cd89c1eeee1dc72220594
SherondaMD <sheronda.mckee@gmail.com>
2023-08-30 15:47:53 -0700

    placed axios import in search and removed weather component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import "./App.css";
 import React from "react";
-import Weather from "./Weather";
 
 export default function App() {
   return (
--- src/Search.js
+++ src/Search.js
@@ -1,6 +1,17 @@
 import React from "react";
+import axios from "axios";
 
 export default function Search(props) {
+  function handleResponse(response) {
+    alert(
+      `The weather in ${response.data.name} is ${response.data.main.temp} degrees F`
+    );
+  }
+
+  let apiKey = "0f8bc384a7c31b717a18cfe38a95ae06";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=imperial`;
+
+  axios.get(apiUrl).then(handleResponse);
   return (
     <div>
       <form className="mb-3">
